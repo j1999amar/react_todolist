966caa7ee65865b0537b0470666191ef5f03dc1f
j1999amar <j1999amar@gmail.com>
2022-06-20 22:23:53 +0530

    todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let addtask = () => {
+    settodoList([
+      ...todoList,
+      { id: todoList.length + 1, name: `${tasklist}`, isDone: false },
+    ]);
+  };
+  let taskdone = (id) => {
+    let itemList = todoList.findIndex((obj) => obj.id === id);
+    // todoList[itemList].isDone = true;
+    settodoList([...todoList]);
+    if (todoList[itemList].isDone === false) {
+      todoList[itemList].isDone = true;
+    } else if (todoList[itemList].isDone == true) {
+      todoList[itemList].isDone = false;
+    }
+  };
+  let deletetask = (id) => {
+    let removetask = todoList.findIndex((obj) => obj.id === id);
+    todoList.splice(removetask, 1);
+    settodoList([...todoList]);
+  };
+
+  const [tasklist, setTaskList] = useState("");
+  const [todoList, settodoList] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <h1>TO DO LIST</h1>
+      <input type="text" onChange={(e) => setTaskList(e.target.value)}></input>{" "}
+      <button onClick={addtask}>Add Task</button>
+
+      <ul>
+        {todoList.map((item) => {
+          return (
+            <li  className={item.isDone ? "markdone" : ""}>
+              <h5>{item.name}</h5>
+              <div><button onClick={() => taskdone(item.id)}>Done</button>
+              <button onClick={() => deletetask(item.id)}>Delete</button>
+              </div>
+              
+              <br></br>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
